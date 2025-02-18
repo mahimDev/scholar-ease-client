e5e369081fcc22a5064c2d6763a99496ed33cea4
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2025-02-18 21:46:54 +0600

    added my faq section

--- src/Pages/Home/FAQ/FAQ.jsx
+++ src/Pages/Home/FAQ/FAQ.jsx
@@ -20,46 +20,35 @@ const FAQ = () => {
     },];
 
     // toggle state and function
+
     const [isOpen, setIsOpen] = useState(null);
-    const handleToggle = (idx) => {
-        setIsOpen((prevIdx) => (prevIdx === idx ? null : idx));
+    const toggle = (idx) => {
+        setIsOpen((prevIdx) => (prevIdx === idx ? null : idx))
     };
     return (
-        <div>
-            {/*  */}
-            <div className="flex w-full justify-center">
-                <div className="max-w-[500px]  cursor-pointer space-y-6">
-                    {/* mapping each accordion  */}
-                    {dataArr.map((data, idx) => (
-                        <div key={idx} onClick={() => handleToggle(idx)} className="flex items-center">
-                            {/* the index div  */}
-                            <div className="flex size-16 select-none items-center justify-center rounded-md bg-zinc-700 text-2xl font-semibold text-white">
-                                <span>0{idx + 1}</span>
-                            </div>
+        <div className="md:flex  w-11/12 mx-auto">
+            <div className="flex-1">
 
-                            <div className="relative h-[2px] w-10 bg-zinc-700">
-                                <span className="absolute -left-2 -top-[5px] z-40 h-3 w-3 rounded-full border-2 border-zinc-700 bg-white"></span>
-                                <span className="h-1 w-10 bg-zinc-700"></span>
-                                <span
-                                    className={`absolute -right-2 -top-[5px] z-40 h-3 w-3 rounded-full border-2 ${isOpen === idx ? 'border-zinc-700 bg-white delay-100' : 'border-transparent'}`}
-                                ></span>
-                            </div>
+                <h1 className="text-6xl font-semibold">frequently asked question</h1>
 
-                            {/* main accordion div  */}
-                            <div className="text-center">
-                                <div className="relative max-w-[450px] border-t-[12px] border-zinc-700 bg-sky-50 p-3 shadow-md">
-                                    <span className="absolute right-0 top-0 h-0 w-0 border-b-[40px] border-r-[40px] border-b-transparent border-r-zinc-700"></span>
-                                    <h1 className="select-none text-lg text-zinc-700">{data.question}</h1>
-                                </div>
-                                <div className={`grid overflow-hidden text-slate-600 transition-all duration-300 ease-in-out ${isOpen === idx ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}>
-                                    <div className="overflow-hidden">
-                                        <div className="max-w-[450px] bg-zinc-700 p-6 text-sm text-white">{data.answer}</div>
-                                    </div>
-                                </div>
-                            </div>
+            </div>
+            <div className="w-full flex-1  rounded-lg bg-white p-3 *:mix-blend-difference ">
+                {dataArr.map((PerAccordion, idx) => (
+                    <div key={idx} className="border-b border-gray-500/50 py-3 last-of-type:border-b-0">
+                        <button onClick={() => toggle(idx)} className="flex h-full w-full items-center justify-between font-medium text-white outline-none">
+                            <span>{PerAccordion.question}</span>
+                            <span className="rounded-full p-2">
+                                <svg className="ml-8 size-3 shrink-0 fill-white" xmlns="http://www.w3.org/2000/svg">
+                                    <rect y="5" width="12" height="2" rx="1" className={`origin-center transform transition duration-200 ease-out ${isOpen === idx && '!rotate-180'}`} />
+                                    <rect y="5" width="12" height="2" rx="1" className={`origin-center rotate-90 transform transition duration-200 ease-out ${isOpen === idx && '!rotate-180'}`} />
+                                </svg>
+                            </span>
+                        </button>
+                        <div className={`grid overflow-hidden text-zinc-400 transition-all duration-300 ease-in-out ${isOpen === idx ? 'grid-rows-[1fr] pb-1 pt-4 opacity-100' : 'grid-rows-[0fr] opacity-0'}`}>
+                            <div className="overflow-hidden pr-4 text-sm">{PerAccordion.answer}</div>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
 
         </div>
@@ -67,3 +56,4 @@ const FAQ = () => {
 };
 
 export default FAQ;
+
